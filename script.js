6a2caa8de6d6f741e607cf3ed5ec0427c92e9756
Resul Elezi <resul_elezi@protonmail.com>
2024-05-15 21:11:24 +0200

    #

--- script.js
+++ script.js
@@ -17,4 +17,5 @@ function isInvalidIput(str) {
 }
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 }
