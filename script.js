41d409be884fd7d40dce781806edd8cf8cf88bcb
Resul Elezi <resul_elezi@protonmail.com>
2024-05-16 21:23:18 +0200

    #

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, "");
 }
-
 function isInvalidIput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
