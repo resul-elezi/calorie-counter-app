103c17c27889e929bb4f8648c407bafb8a852225
Resul Elezi <resul_elezi@protonmail.com>
2024-05-16 19:46:47 +0200

    #

--- script.js
+++ script.js
@@ -18,5 +18,6 @@ function isInvalidIput(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-    const HTMLString = ``;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name">`;
 }
