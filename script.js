fd4d1dcebab0f201ab2ee9bed3cb97a86dcfa7a5
Resul Elezi <resul_elezi@protonmail.com>
2024-05-21 10:58:51 +0200

    #

--- script.js
+++ script.js
@@ -29,6 +29,13 @@ function addEntry() {
     e.preventDefault();
     isError = false;
     const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type=number]");
+    const dinnerNumberInputs = document.querySelectorAll("#dinner input[type=number]");
+    const snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]");
+    const exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]");
+
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
   }
 
   function getCaloriesFromInputs(list) {
