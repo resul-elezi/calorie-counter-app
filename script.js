5e12eaa060f98cbbbba397357231be1e59b8dd19
Resul Elezi <resul_elezi@protonmail.com>
2024-05-23 19:55:24 +0200

    #

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function addEntry() {
     }
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+    const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
     output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
     <hr>
     <p>${budgetCalories} Calories Budgeted</p>
